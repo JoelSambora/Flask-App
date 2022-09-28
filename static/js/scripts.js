2785093cd5a5793b4448ce803e14dd4a81632d9c
JoelSambora <83646392+JoelSambora@users.noreply.github.com>
2022-09-28 20:40:31 +0200

    segunda janela para login com voice

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -51,9 +51,53 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
-
 });
+
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+recognition.lang = 'en-GB';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+const button = document.getElementById("start");
+const input = document.getElementById("search");
+const supportmsg = document.getElementById("support-msg");
+
+if ('SpeechRecognition' in window) {
+  button.classList.add("supported");
+  supportmsg.classList.add("supported");
+}
+
+button.addEventListener("click", function(){
+  recognition.start();
+  button.innerHTML = "Listening...";
+  button.classList.add("glow");
+  console.log('Ready to receive voice input.');
+});
+
+recognition.onresult = function(event) {
+  var last = event.results.length - 1;
+  var searchResult = event.results[last][0].transcript;
+  button.innerHTML = "Click to speak";
+  input.value = searchResult;
+  button.classList.remove("glow");
+  console.log(searchResult);
+}
+
+recognition.onspeechend = function() {
+  recognition.stop();
+}
+
+recognition.onnomatch = function(event) {
+  console.log('Not recognised');
+  button.innerHTML = "Click to speak";
+}
+
+recognition.onerror = function(event) {
+  console.log('Error occurred in recognition: ' + event.error);
+  button.innerHTML = "Click to speak";
+}
