5c6ea787ff9b77c2367fe9590e7d4def6a899287
JoelSambora <83646392+JoelSambora@users.noreply.github.com>
2022-10-24 15:59:16 +0200

    register css

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 from email import message
 import os
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
-
 from flask_wtf import FlaskForm
-import email_validator
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField,SelectField, TelField
 from wtforms.validators import InputRequired, Email, Length
 
 
 app = Flask(__name__)
 
 class RegisterForm(FlaskForm):
-    nome = StringField('Nome', validators=[InputRequired(), Length(min=4, max=24)])
-    email = StringField('Email', validators=[InputRequired(), Email(message='Email invalido'), Length(max=50)])
-    password = PasswordField('Senha', validators=[InputRequired(), Length(min=8, max=15), ], render_kw={"placeholder": "test"})
+
+    nome = StringField('Nome', validators=[InputRequired(), Length(min=4, max=24)], render_kw={"placeholder": "Nome completo"})
+    email = StringField('Email', validators=[InputRequired(), Email(message='Email invalido'), Length(max=50)], render_kw={"placeholder": "anavoice@ana.com"})
+    provincia = SelectField(u'Província', choices=[('MC', 'Maputo Cidade'), ('MP', 'Maputo Província')])
+    avenida = StringField('Avenida', validators=[InputRequired(), Length(min=4, max=24)], render_kw={"placeholder": "Avenida"})
+    bairro = StringField('bairro', validators=[InputRequired(), Length(min=4, max=24)], render_kw={"placeholder": "bairro"})
+    telefone = TelField('Número de telefone', [InputRequired(), Length(min=9, max=9)], render_kw={"placeholder": "840000000"})
+    casaNumero = TelField('Número da casa', [InputRequired(), Length(min=1, max=9)])
+    password = PasswordField('Senha', validators=[InputRequired(), Length(min=8, max=15), ], render_kw={"placeholder": "Senha do colaborador"})
 
 class LoginForm(FlaskForm):
-    email = StringField('',validators=[InputRequired(), Email(message='Email invalido'), Length(max=50)])
-    password = PasswordField('', validators=[InputRequired(), Length(min=8, max=15)])
+    email = StringField('',validators=[InputRequired(), Email(message='Email invalido'), Length(max=50)], render_kw={"placeholder": "Seu email"})
+    password = PasswordField('', validators=[InputRequired(), Length(min=8, max=15)], render_kw={"placeholder": "Sua senha"})
 #index /  pagina inicial
 @app.route('/')
 def index():
@@ -47,6 +51,15 @@ def create():
     
     return render_template("accounts/register.html", form = form)
 
+
+app.config["UPLOAD_DIR"] = "uploads"
+@app.route("/upload", methods = ["GET", "POST"])
+def upload():
+    if request.method == 'POST':
+        for file in request.files.getlist('file'):
+             file.save(os.path.join(app.config['UPLOAD_DIR'], file.filename))
+        return render_template("accounts/register.html", msg = "File uplaoded successfully.")
+    return render_template("accounts/register", msg = "")
 # função que retorna erro para página que não existe
 @app.route('/<string:nome>')
 def error(nome):
